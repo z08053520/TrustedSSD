f7556b68d14dd7a8aa37d3d43925de811c6bf3a4
Tate, Hongliang Tian <tatetian@gmail.com>
2014-01-06 00:37:27 +0800

    Add request queue and slab

---
+++ OpenSSD/1.0.6/ftl_tssd/request_queue.c
@@ -0,0 +1,107 @@
+#include "request_queue.h"
+
+/* ===========================================================================
+ *  Public Interface
+ * =========================================================================*/
+
+#define reset_req_in_queue(req)		(req->req_id = NULL_REQ_ID, \
+					 req->waiting_banks = 0)
+#define REQ_QUEUE_SIZE		32
+static request_in_queue_t _queue[REQ_QUEUE_SIZE];
+static UINT8		_queue_head;
+static UINT8		_queue_size;
+
+void 	request_queue_init()
+{
+	*g_request_handler = NULL;
+	
+	_queue_head = 0;
+	_queue_size = 0;
+
+	UINT8 i;
+	for (i = 0; i < REQ_QUEUE_SIZE; i++)
+		reset_req_in_queue(&_queue[i]);
+}
+
+BOOL8	request_queue_is_full()
+{
+	return _queue_size == REQ_QUEUE_SIZE;
+}
+
+void	request_queue_accept_new(request_id_t const req_id, 
+				 banks_mask_t *idle_banks)
+{
+	BUG_ON("queue is full; can't accept any new request",
+		request_queue_is_full());
+
+	UINT8 queue_tail = (_queue_head + _queue_size) % REQ_QUEUE_SIZE;
+	request_in_queue_t *req = & _queue[queue_tail];
+	req->req_id = req_id;
+	req->waiting_banks = 0;
+
+	(*g_request_handler)(req, idle_banks);
+}
+
+#define FOR_EACH_REQUEST_IN_QUEUE(req)					\
+	for (queue_idx = _queue_head, 					\
+	     queue_end = (_queue_head + _queue_size) % REQ_QUEUE_SIZE,	\
+	     req = &_queue[_queue_idx]; 				\
+	     queue_idx < queue_end; 					\
+	     queue_idx = (queue_idx + 1) % REQ_QUEUE_SIZE		\
+	     req = &_queue[_queue_idx]) 
+
+void	request_queue_process_old(banks_mask_t *idle_banks)
+{
+	UINT8 queue_end, queue_idx;
+	request_in_queue_t *req;
+	// Process old requests
+	FOR_EACH_REQUEST_IN_QUEUE(req) {
+		if ((*idle_banks) == 0) break;
+
+		if ((req->waiting_banks & (*idle_banks)) == 0) continue;
+		
+		// process the request if possible
+		(*g_request_handler)(req, idle_banks); 
+	}
+
+	// Update BM read limit 
+	UINT8 done_rd_requests = 0;
+	FOR_EACH_REQUEST_IN_QUEUE(req) {
+		if (req->rw == WR_REQ) continue;
+
+		if (req->waiting_banks != 0) break;
+		done_rd_requests++;
+	}
+	if (done_rd_requestsa) {
+		UINT32 new_rd_limit = (GETREG(BM_READ_LIMIT) + done_rd_requests) 
+				    % NUM_SATA_RD_BUFFERS;
+		SETREG(BM_STACK_RDSET, new_rd_limit);
+		SETREG(BM_STACK_RESET, 0x02);
+	}
+
+	// Update BM write limit
+	UINT8 done_wr_requests = 0;
+	FOR_EACH_REQUEST_IN_QUEUE(req) {
+		if (req->rw == RD_REQ) continue;
+
+		if (req->waiting_banks != 0) break;
+		done_wr_requests++;
+	}
+	if (done_wr_requestsa) {
+		UINT32 new_wr_limit = (GETREG(BM_WRITE_LIMIT) + done_wr_requests) 
+				    % NUM_SATA_WR_BUFFERS;
+		SETREG(BM_STACK_WRSET, new_wr_limit);
+		SETREG(BM_STACK_RESET, 0x01);
+	}
+
+	// Update queue head and size
+	UINT8 done_requests = 0;
+	FOR_EACH_REQUEST_IN_QUEUE(req) {
+		if (req->waiting_banks != 0) break;
+		done_requests++;
+	}
+	if (done_requests) {
+		_queue_head = (_queue_head + done_requests) % REQ_QUEUE_SIZE;
+		_queue_size -= done_requests;
+	}
+}
---
+++ OpenSSD/1.0.6/ftl_tssd/slab.h
@@ -0,0 +1,45 @@
+#ifndef __SLAB_H
+#define __SLAB_H
+/* *
+ * Slab 
+ *
+ * Preallocated contiguous memory for efficient allocation and deallocation 
+ * for objects of specific type. The concept was borrowed from Linux kernel.
+ * */
+
+#define	define_slab_interface(name, type)			\
+		type*	allocate_**name();			\
+		void 	deallocate_**name(type*);		\
+		void 	init_slab_**name();
+		
+#define define_slab_implementation(name, type, capacity)		\
+		typedef struct _slab_**name**_obj {			\
+			type				_payload;	\
+			struct _slab_**name**_obj	*next_free;	\
+		} slab_**name**_obj_t;					\
+		static slab_**name**_obj_t	 slab_**name**_buf[capacity];	\
+		static UINT32			 slab_**name**_size;		\
+		static slab_**name**_obj_t 	*slab_**name**_free_list;	\
+		static void init_slab_**name() {			\
+			slab_**name**_size = 0;				\
+			slab_**name**_free_list = NULL;			\
+			UINT32 i = 0;					\
+			for (i = 0; i < capacity; i++)			\
+				slab_**name**_buf[i].next_free = NULL;	\
+		}							\
+		static type* allocate_**name() {			\
+			if (slab_**name**_free_list) {			\
+				slab_**name**_obj_t	*free_obj;	\
+				free_obj = slab_**name**_free_list;	\
+				slab_**name**_free_list = free_obj->next_free;	\
+				return (type*) free_obj;		\
+			}						\
+			if (slab_**name**_size == capacity) return NULL;	\
+			return slab_**name**_buf[slab_**name**_size++];	\
+		}							\
+		static void deallocate_**name(type* obj) {		\
+			slab_**name**_obj_t *slab_obj = obj;		\
+			slab_obj->next_free = slab_**name**_free_list;	\
+			slab_**name**_free_list = slab_obj;		\
+		}
+#endif
