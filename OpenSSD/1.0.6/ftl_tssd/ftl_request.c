a92060f6484a5352e66d39114b775a7aa0469756
Tate, Hongliang Tian <tatetian@gmail.com>
2014-01-08 14:39:16 +0800

    Refactor flt_request; now a framework for handlers is ready

--- OpenSSD/1.0.6/ftl_tssd/ftl_request.c
+++ OpenSSD/1.0.6/ftl_tssd/ftl_request.c
@@ -5,15 +5,19 @@
  * Macros and Types 
  * =========================================================================*/
 
+extern UINT32 g_ftl_read_buf_id, g_ftl_write_buf_id;
+
 /* TODO: use gcc's -fshort-enums to make enum use as less bits as possible */
 typedef enum {
-	REQ_STATE_NEW,
-	REQ_STATE_FINISH,
-	NUM_REQ_STATES
-} request_state_t;
+	REQ_PHASE_PREPARATION,
+	REQ_PHASE_MAPPING,
+	REQ_PHASE_FLASH,
+	REQ_PHASE_FINISH,
+	NUM_REQ_PHASES
+} request_phase_t;
 
 typedef struct request {
-	request_state_t		state;
+	request_phase_t		phase;
 	UINT32			lpn;
 	UINT8			offset;
 	UINT8			num_sectors;
@@ -24,43 +28,111 @@ define_slab_interface(request, request_t);
 define_slab_implementation(request, request_t, REQ_QUEUE_SIZE);
 
 /* ===========================================================================
- * Request Handler (core of FTL) 
+ * Request Handler (Declaration) 
  * =========================================================================*/
 
-static request_t* id2req(request_id_t const id)
-{
-	return & slab_request_buf[id]; 
-}
+#define HANDLER_CONTINUE	0
+#define HANDLER_EXIT		1
 
-static void read_handler(request_in_queue_t *req_in_queue, banks_mask_t *idle_banks)
-{
-	request_t *req = id2req(req_in_queue->req_id);
-	switch(req->state) {
-	REQ_STATE_NEW:
-		break;
-	REQ_STATE_FINISH:
-		break;
-	}
-}
+typedef BOOL8 	(*phase_handler)(request_t *req, 
+			 	 banks_mask_t *waiting_banks,
+			 	 banks_mask_t *idle_banks);
 
-static void write_handler(request_in_queue_t *req_in_queue, banks_mask_t *idle_banks)
-{
-	request_t *req = id2req(req_in_queue->req_id);
-	switch(req->state) {
-	REQ_STATE_NEW:
-		break;
-	REQ_STATE_FINISH:
-		break;
-	}
-}
+/* #define declare_phase_handler(name)					\ */
+/* 		static BOOL8 name**_phase(				\ */
+/* 				request_t* req,				\ */
+/* 				banks_mask_t* waiting_banks, 		\ */
+/* 				banks_mask_t* idle_banks); */
 
+static BOOL8 read_preparation_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 read_mapping_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 read_flash_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 read_finish_phase(request_t*, banks_mask_t*, banks_mask_t*);
+
+static BOOL8 write_preparation_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 write_mapping_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 write_flash_phase(request_t*, banks_mask_t*, banks_mask_t*);
+static BOOL8 write_finish_phase(request_t*, banks_mask_t*, banks_mask_t*);
+
+phase_handler	handlers_for_read[NUM_REQ_PHASES] = {
+	read_preparation_phase,
+	read_mapping_phase,
+	read_flash_phase,
+	read_finish_phase
+};
+
+phase_handler	handlers_for_write[NUM_REQ_PHASES] = {
+	write_preparation_phase,
+	write_mapping_phase,
+	write_flash_phase,
+	write_finish_phase
+};
+
+#define id2req(id)		(& slab_request_buf[(id)])
+
+/* Main handler, invoked by request_queue to handle all requests */
 static void ftl_request_handler(request_in_queue_t *req_in_queue, banks_mask_t *idle_banks)
 {
-	if (req_in_queue->rw == READ)
-		read_handler (req_in_queue, idle_banks);
-	else
-		write_handler(req_in_queue, idle_banks);
+	phase_handler* handlers = req_in_queue->rw == READ ?
+					handlers_for_read :
+					handlers_for_write;
+	request_t *req = id2req(req_in_queue->req_id);
+	banks_mask_t *waiting_banks = & req_in_queue->waiting_banks;
+
+	BOOL8 res;
+	do {
+		res = (*(handlers[req->phase]))(req, waiting_banks, idle_banks);
+	} while (res == HANDLER_CONTINUE);  
 }
+
+/* ===========================================================================
+ * Phase Handlers for Read 
+ * =========================================================================*/
+
+static BOOL8 read_preparation_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 read_mapping_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 read_flash_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 read_finish_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+/* ===========================================================================
+ * Phase Handlers for Read 
+ * =========================================================================*/
+
+static BOOL8 write_preparation_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 write_mapping_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 write_flash_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
+static BOOL8 write_finish_phase(request_t*, banks_mask_t*, banks_mask_t*)
+{
+	return HANDLER_EXIT;
+}
+
 
 /* ===========================================================================
  *  Public Interface
@@ -77,7 +149,7 @@ request_t* allocate_and_init_request(UINT32 const lpn, UINT8 const offset,
 				     UINT8  const num_sectors)
 {
 	request_t *req = allocate_request();
-	req->state = REQ_STATE_NEW;
+	req->phase = REQ_PHASE_PREPARATION;
 
 	req->lpn = lpn;
 	req->offset = offset;
