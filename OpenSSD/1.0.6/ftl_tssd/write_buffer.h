264ea08ee92e386185338ed1f9fa756a03c7d3ef
Tate, Hongliang Tian <tatetian@gmail.com>
2014-01-15 00:05:01 +0800

    Minor changes to write_buffer interface

--- OpenSSD/1.0.6/ftl_tssd/write_buffer.h
+++ OpenSSD/1.0.6/ftl_tssd/write_buffer.h
@@ -4,7 +4,7 @@
 #include "jasmine.h"
 
 void write_buffer_init();
-BOOL8 write_buffer_is_full();
+
 void write_buffer_get(UINT32 const lpn, 
 		      UINT32 *buf, 
 		      sectors_mask_t *valid_sectors);
@@ -12,7 +12,11 @@ void write_buffer_put(UINT32 const lpn,
 		      UINT8  const sector_offset, 
 		      UINT8  const num_sectors,
 		      UINT32 const sata_wr_buf);
+
 void write_buffer_drop(UINT32 const lpn);
-void write_buffer_flush(UINT32 const buf, UINT32 *lspn, 
+
+BOOL8 write_buffer_is_full();
+void write_buffer_flush(UINT32 const buf, 
+			UINT32 *lspn, 
 			sectors_mask_t *valid_sectors);
 #endif
