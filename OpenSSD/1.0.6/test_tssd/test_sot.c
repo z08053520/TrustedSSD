a838b441f3043192abd8bcac6aa9978b5b5f2aa9
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-08 22:16:33 +0800

    Add SOT unit test

--- OpenSSD/1.0.6/test_tssd/test_sot.c
+++ OpenSSD/1.0.6/test_tssd/test_sot.c
@@ -1,5 +1,5 @@
 /* ===========================================================================
- * Unit test for Sector Ownership Table 
+ * Unit test for Sector Ownership Table
  * =========================================================================*/
 #include "jasmine.h"
 #if OPTION_FTL_TEST
@@ -8,67 +8,103 @@
 	#error ACL is not enabled
 #endif
 
+#include "dram.h"
 #include "sot.h"
-#include "ftl.h"
+#include "mem_util.h"
 #include "test_util.h"
+#include "sata.h"
 #include <stdlib.h>
 
-#define LBA_BUF_ADDR	TEMP_BUF_ADDR
-#define LBA_BUF_SIZE	SECTORS_PER_PAGE
-#define UID_BUF_ADDR	HIL_BUF_ADDR
-#define UID_BUF_SIZE	SECTORS_PER_PAGE
+#define RAND_SEED	1234
 
-SETUP_BUF(lba, LBA_BUF_ADDR, LBA_BUF_SIZE);
-SETUP_BUF(uid, UID_BUF_ADDR, UID_BUF_SIZE);
+#define LBA_BUF		TEMP_BUF_ADDR
+#define UID_BUF		HIL_BUF_ADDR
 
-#define SAMPLE_SIZE 	(BYTES_PER_PAGE / sizeof(UINT32))
+#define BUF_SIZE	(BYTES_PER_PAGE / sizeof(uid_t))
+#define SAMPLE_SIZE	BUF_SIZE
 
-#define RAND_SEED	123456
+SETUP_BUF(lba,		LBA_BUF,	SECTORS_PER_PAGE);
+SETUP_BUF(vp,		UID_BUF,	SECTORS_PER_PAGE);
 
-#define MAX_UID		0xFFFF
+static void load_sot(UINT32 const lpn)
+{
+	BOOL8		idle = FALSE;
+	task_res_t	res  = sot_load(lpn);
+	while (res != TASK_CONTINUED) {
+		BUG_ON("task engine is idle, but sot page haven't loaded", idle);
+		idle = task_engine_run();
+		res  = sot_load(lpn);
+	}
+}
 
 void ftl_test()
 {
-	uart_print("Running unit test for SOT...");
-	
-	UINT32 lba;
-	uid_t  uid;
-	UINT32 i;
-	UINT32 j, repeats = 4;
+	UINT32	i, j;
+	UINT32	lba, lpn;
+	UINT8	offset, num_sectors;
+	uid_t	uid;
+
+	uart_print("Running unit test for SOT... ");
+	uart_printf("sample size = %d\r\n", SAMPLE_SIZE);
 
 	srand(RAND_SEED);
-
-	init_lba_buf(0);
+ 	init_lba_buf(0xFFFFFFFF);
 	init_uid_buf(0);
 
-	uart_print("Randomly check SOT initial state");
-	for (i = 0; i < SAMPLE_SIZE; i++) {
-		lba = random(0, MAX_LBA);
-		sot_fetch(lba, &uid);
-		BUG_ON("SOT entries is not initialized as zeros", uid != 0);
+	uart_print("\tcheck default uid for sectors is 0");
+	for(i = 0; i < SAMPLE_SIZE; i++) {
+		lba = rand() % MAX_LBA;
+		lpn = lba / SECTORS_PER_PAGE;
+		offset = lba % SECTORS_PER_PAGE;
+		num_sectors = random(1, SECTORS_PER_PAGE - offset);
+
+		load_sot(lpn);
+		BOOL8 ok = sot_authenticate(lpn, offset, num_sectors, 0);
+		BUG_ON("default uid is not ZERO!!!", !ok);
 	}
 
-	for (j = 0; j < repeats; j++) {
-		uart_print("Randomly update SOT entries");
-		for (i = 0; i < SAMPLE_SIZE; i++) {
-			lba = random(0, MAX_LBA);
-			uid = random(0, MAX_UID);
-			sot_update(lba, uid);
+	uart_print("\tupdate uid of sector");
+	i = 0;
+	while (i < SAMPLE_SIZE) {
+		lba = rand() % MAX_LBA;
+		lpn = lba / SECTORS_PER_PAGE;
+		offset = lba % SECTORS_PER_PAGE;
+		num_sectors = random(1, SECTORS_PER_PAGE - offset);
+		uid = (uid_t) rand();
 
-			set_lba(i, lba);
-			set_uid(i, uid);
-		}
+		load_sot(lpn);
+		sot_authorize(lpn, offset, num_sectors, uid);
 
-		uart_print("Verify the update SOT entries");
-		for (i = 0; i < SAMPLE_SIZE; i++) {
-			lba = get_lba(i);
-			sot_fetch(lba, &uid);
-//			uart_printf("actual uid = %u, expected uid = %u", uid, get_uid(i));
-			BUG_ON("SOT entry is not as expected", uid != get_uid(i));
+		UINT32 lba_end = lba + num_sectors;
+		for (; lba < lba_end; lba++) {
+			j 	= mem_search_equ_dram(LBA_BUF, sizeof(UINT32),
+						      BUF_SIZE, lba);
+			if (j < BUF_SIZE) {
+				set_uid(j, uid);
+			}
+			else {
+				if (i >= SAMPLE_SIZE) break;
+				set_lba(i, lba);
+				set_uid(i, uid);
+				i++;
+			}
 		}
 	}
-	
-	uart_print("Unit test for SOT passed ^_^");
+
+	uart_print("\tverify lba->uid");
+	for(i = 0; i < SAMPLE_SIZE; i++) {
+		lba = get_lba(i);
+		uid = get_uid(i);
+
+		lpn = lba / SECTORS_PER_PAGE;
+		offset = lba % SECTORS_PER_PAGE;
+		load_sot(lpn);
+		BOOL8 ok = sot_authenticate(lpn, offset, 1, uid);
+		/*  uart_printf("vp (bank, vpn) = (%u, %u); expected_vp (bank, vpn) = (%u, %u)\r\n",
+			    vp.bank, vp.vpn, expected_vp.bank, expected_vp.vpn);*/
+		BUG_ON("authentication failuire", !ok);
+	}
+
+	uart_print("SOT passed the unit test ^_^");
 }
-
 #endif
