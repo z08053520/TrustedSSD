f182915f2decd56fde241d06cedca92ce2eb1e62
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-13 23:10:08 +0800

    Add to last commit

--- OpenSSD/1.0.6/ftl_tssd/ftl_read_task.c
+++ OpenSSD/1.0.6/ftl_tssd/ftl_read_task.c
@@ -358,7 +358,7 @@ void ftl_read_task_register()
 
 void ftl_read_task_init(task_t *task,
 #if	OPTION_ACL
-			UINT32 const uid,
+			user_id_t const uid,
 #endif
 			UINT32 const lpn,
 		   	UINT8 const offset, UINT8 const num_sectors)
