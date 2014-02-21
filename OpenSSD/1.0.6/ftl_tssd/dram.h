8f0f13e1d7e48177c50c464793818dd6f8243ec8
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-21 20:08:48 +0800

    In the middle of fixing a bug related to SOT

--- OpenSSD/1.0.6/ftl_tssd/dram.h
+++ OpenSSD/1.0.6/ftl_tssd/dram.h
@@ -53,7 +53,7 @@
 #define TASK_SWAP_ADDR		GTD_END
 #define TASK_SWAP_BYTES		BYTES_PER_PAGE
 #define TASK_SWAP_END		(TASK_SWAP_ADDR + TASK_SWAP_BYTES)
-#define MAX_NUM_TASKS		16
+#define MAX_NUM_TASKS		4
 #define SWAP_BYTES_PER_TASK	(TASK_SWAP_BYTES / MAX_NUM_TASKS)
 #define TASK_SWAP_BUF(task_id)	(TASK_SWAP_ADDR + (task_id) * SWAP_BYTES_PER_TASK)
 
@@ -71,7 +71,7 @@
  * ========================================================================= */
 
 #define NUM_READ_BUFFERS	1
-#define NUM_WRITE_BUFFERS	8
+#define NUM_WRITE_BUFFERS	2
 
 #define READ_BUF_ADDR		PC_FLUSH_BUF_END
 #define READ_BUF_BYTES		(NUM_READ_BUFFERS * BYTES_PER_PAGE)
