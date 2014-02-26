725f9b353340ddff60ba8c86ad3a3a323bb136b1
Tate, Hongliang Tian <tatetian@gmail.com>
2014-02-26 14:08:06 +0800

    Minor change to GTD

--- OpenSSD/1.0.6/ftl_tssd/gtd.h
+++ OpenSSD/1.0.6/ftl_tssd/gtd.h
@@ -5,24 +5,24 @@
 
 /* *
  * GTD = global translation table
- * 	
+ *
  * 		PMT/SOT subpage index --> virtual sub page
  *
- *   GTD serves the logical-to-physical address translation for pages of PMT 
- *   (Page Mapping Table) and SOT (Secotr Ownership Table). While the address
- *   of user pages are translated to by looking up PMT.
+ *   GTD serves the logical-to-physical address translation for pages of PMT
+ *   (Page Mapping Table) and SOT (Secotr Ownership Table). Note that the
+ *   address of user pages are translated to by looking up PMT.
  *
- *   In contrast to user pages, PMT and SOT pages (which are 4KB) are mapped to 
+ *   In contrast to user pages, PMT and SOT pages (which are 4KB) are mapped to
  *   virtual sub-pages in a fully assoiciative way.
  *
  *   Let's do some simple math to calculate the number of entries in GTD.
- *   For a 64GB flash with 32KB page and 4KB sub-page, there are 2K PMT pages 
- *   and 8K SOT pages; In other word, a total 10K GTD entries, in 
+ *   For a 64GB flash with 32KB page and 4KB sub-page, there are 2K PMT pages
+ *   and 8K SOT pages; In other word, a total 10K GTD entries, in
  *   10K / (32KB / sizeof(UINT32)) = 1.25 page < 2 page.
  * */
 
 /* ===========================================================================
- * Macro Definitions 
+ * Macro Definitions
  * =========================================================================*/
 
 #if OPTION_ACL
@@ -45,8 +45,8 @@ typedef enum {
 /* may require gcc to use -fms-extensions */
 typedef union {
 	struct {
-		UINT8	type:1;
-		UINT32	idx:31;
+		page_type_t	type:2;
+		UINT32		idx:30;
 	};
 	UINT32	as_uint;
 } page_key_t;
@@ -58,16 +58,16 @@ typedef struct {
 #define ENTRY(zone_name)	UINT32 zone_name[zone_name##_SUB_PAGES];
 	PAGE_TYPE_LIST
 #undef ENTRY
-} gtd_zones_t; 
+} gtd_zones_t;
 
 /* ===========================================================================
- * Public Interface 
+ * Public Interface
  * =========================================================================*/
 
 void gtd_init(void);
 void gtd_flush(void);
 
 vsp_t  gtd_get_vsp(page_key_t const key);
-void   gtd_set_vsp(page_key_t const key, vsp_t const vsp); 
+void   gtd_set_vsp(page_key_t const key, vsp_t const vsp);
 
 #endif /* __GTD_H */
